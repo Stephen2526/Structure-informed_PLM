cb83b735000d4dd8eac07f084eba04ee35f6143b
sunyuanfei <sunyuanfei@tamu.edu>
2024-01-26 00:34:57 -0600

    Remove ignored folder

--- data_scripts/create_mutation_list_sets.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for i in $(seq 1 20)
-do
-  pcregrep -M -o1 '^.+BLAT_ECOLX\s([A-Z]\d*[A-Z])' folds/BLAT_ECOLX_Scratch.fasta_set_$i > mutListFolds/BLAT_ECOLX_mutList.fasta_set_$i
-done
-
